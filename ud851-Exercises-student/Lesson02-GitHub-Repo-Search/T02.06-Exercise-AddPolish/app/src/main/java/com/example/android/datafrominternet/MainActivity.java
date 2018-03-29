ed5e2bacf9c5654a668b573bb98a702b72a82207
Rafael Brugiolo <brugiolo@hotmail.com>
2018-03-29 10:08:35 -0300

    Include of exercise add polish

--- ud851-Exercises-student/Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ ud851-Exercises-student/Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -36,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mSearchResultsTextView;
 
+    // TODO (12) Create a variable to store a reference to the error message TextView
+
+    // TODO (24) Create a ProgressBar variable to store a reference to the ProgressBar
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,43 +49,53 @@ public class MainActivity extends AppCompatActivity {
 
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+
+        // TODO (13) Get a reference to the error TextView using findViewById
+
+        // TODO (25) Get a reference to the ProgressBar using findViewById
     }
 
     /**
-     * This method retrieves the search text from the EditText, constructs the URL
-     * (using {@link NetworkUtils}) for the github repository you'd like to find, displays
+     * This method retrieves the search text from the EditText, constructs the
+     * URL (using {@link NetworkUtils}) for the github repository you'd like to find, displays
      * that URL in a TextView, and finally fires off an AsyncTask to perform the GET request using
-     * our (not yet created) {@link GithubQueryTask}
+     * our {@link GithubQueryTask}
      */
     private void makeGithubSearchQuery() {
         String githubQuery = mSearchBoxEditText.getText().toString();
         URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
         mUrlDisplayTextView.setText(githubSearchUrl.toString());
-        mSearchResultsTextView.setText("Searching...");
-
         new GithubQueryTask().execute(githubSearchUrl);
     }
 
-    public class GithubQueryTask extends AsyncTask<URL, Void, String>{
-        @Override
-        protected String doInBackground(URL... urls) {
-            URL searchUrl = urls[0];
-            String githubSerachResults = null;
+    // TODO (14) Create a method called showJsonDataView to show the data and hide the error
 
-            try{
-                githubSerachResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
-            }catch(IOException e){
+    // TODO (15) Create a method called showErrorMessage to show the error and hide the data
+
+    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+
+        // TODO (26) Override onPreExecute to set the loading indicator to visible
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String githubSearchResults = null;
+            try {
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            return githubSerachResults;
+            return githubSearchResults;
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            if (s != null & s != ""){
-                mSearchResultsTextView.setText(s);
+        protected void onPostExecute(String githubSearchResults) {
+            // TODO (27) As soon as the loading is complete, hide the loading indicator
+            if (githubSearchResults != null && !githubSearchResults.equals("")) {
+                // TODO (17) Call showJsonDataView if we have valid, non-null results
+                mSearchResultsTextView.setText(githubSearchResults);
             }
+            // TODO (16) Call showErrorMessage if the result is null in onPostExecute
         }
     }
 
